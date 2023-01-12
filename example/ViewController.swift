1d222b92bc0bf0aaadde348f7d391fee2f56f4d7
Stefan Nebel <stefan.nebel@s-markt-mehrwert.de>
2023-01-12 09:54:25 +0100

    Initial commit

--- example/ViewController.swift
+++ example/ViewController.swift
@@ -5,15 +5,36 @@
 //  Created by Stefan Nebel on 12.01.23.
 //
 
+import SnapKit
 import UIKit
 
 class ViewController: UIViewController {
 
+  private let oneLineButton = UIButton()
+
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view.
+
+    setupView()
   }
 
+  private func setupView() {
+    // self
+    view.backgroundColor = .systemGroupedBackground
 
+    // oneLineButton
+    view.addSubview(oneLineButton)
+    oneLineButton.configuration = .plain()
+    oneLineButton.titleLabel?.numberOfLines = 1
+    oneLineButton.subtitleLabel?.numberOfLines = 1
+    oneLineButton.setTitle(
+      "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua."
+      , for: .normal
+    )
+    oneLineButton.snp.makeConstraints({
+      $0.centerY.equalToSuperview()
+      $0.trailing.leading.equalToSuperview()
+    })
+  }
 }
 
